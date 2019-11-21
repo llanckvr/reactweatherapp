89b62cd95b88d3b17b21a9588630994452cb6468
llanckvr <56133069+llanckvr@users.noreply.github.com>
2019-11-21 19:47:21 +0000

    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import Loader from "react-loader-spinner";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.orghttps://github.com/llanckvr/reactweatherapp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open source Code
-        </a>
-        <p>By Laure-Anne Lanckvrind</p>
+      <div className="Container">
+        <small>
+          <a
+            className="App-link"
+            href="https://reactjs.orghttps://github.com/llanckvr/reactweatherapp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open source Code
+          </a>
+          <p>By Laure-Anne Lanckvrind</p>
+        </small>
         <Loader
           type="Puff"
           color="#00BFFF"
@@ -24,7 +25,7 @@ function App() {
           width={100}
           timeout={3000} //3 secs
         />
-      </header>
+      </div>
     </div>
   );
 }
