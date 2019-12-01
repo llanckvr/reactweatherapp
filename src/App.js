728feac83b8944b93857bbbc89aad623b53ff036
llanckvr <56133069+llanckvr@users.noreply.github.com>
2019-12-01 12:54:48 +0000

    Imported WeatherImage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Loader from "react-loader-spinner";
+import WeatherImage from "./weatherImage";
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
+        <form>
+          <i className="fas fa-search "></i>
+          <input
+            id="city-input"
+            type="text"
+            placeholder="Search"
+            className="location-search"
+          />
+        </form>
+        <br />
+
+        <div className="row">
+          <div className="col-sm"></div>
+          <div className="col-sm">
+            <small id="actual-time" className="actual-time">
+              {" "}
+              MON 10:30 AM{" "}
+            </small>
+          </div>
+          <div className="col-sm"></div>
+        </div>
+        <h1 id="city-name"> - </h1>
+        <WeatherImage />
+
+        <h2 className="main-temperature">
+          {" "}
+          <span>-</span> <span className="celsius">ºC </span>{" "}
+          <span className="fahrenheit">|ºF </span>{" "}
+        </h2>
+        <p>
+          {" "}
+          <span className="actual-coverage"> - </span>
+        </p>
+
+        <div className="row">
+          <div className="col-sm">
+            <i className="fas fa-tint actual-humidity"></i>
+            <br />
+            <span> - </span>
+          </div>
+          <div className="col-sm">
+            <i className="fas fa-wind actual-wind"></i>
+            <br />
+            <span>-</span>
+          </div>
+          <div className="col-sm">
+            <i className="fas fa-cloud-showers-heavy actual-precipitation"></i>
+            <br />
+            <span> - </span>
+          </div>
+        </div>
+      </div>
+
+      <footer>
         <small>
           <a
             className="App-link"
@@ -18,14 +72,14 @@ function App() {
           </a>
           <p>By Laure-Anne Lanckvrind</p>
         </small>
-        <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={3000} //3 secs
-        />
-      </div>
+      </footer>
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
     </div>
   );
 }
